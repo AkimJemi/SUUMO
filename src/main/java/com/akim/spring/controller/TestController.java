efa7436672712653be543b89ce03dc3484ff5517
AkimJemi <rlawoals623@gmail.com>
2023-10-01 21:24:44 +0900

    20231001 cookies

--- src/main/java/com/akim/spring/controller/TestController.java
+++ src/main/java/com/akim/spring/controller/TestController.java
@@ -1,15 +1,50 @@
 package com.akim.spring.controller;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.tomcat.util.buf.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.akim.spring.util.SuumoRequestUtil;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+
 @Controller
+@RequiredArgsConstructor
 public class TestController {
+
+    private final SuumoRequestUtil ut;
 
     @RequestMapping("test")
     public ModelAndView test(ModelAndView mv) {
         mv.setViewName("NewFile");
         return mv;
+    }
+
+    @RequestMapping("cookies")
+    @ResponseBody
+    public String cookies(ModelAndView mv) {
+        return ut.getAllCookieValuesAsString();
+    }
+
+    @RequestMapping("setCookies")
+    public String setCookies(HttpServletRequest rq) {
+        Map<String, String[]> params = rq.getParameterMap();
+        for (Entry<String, String[]> element : params.entrySet()) {
+            System.out.println(element.getValue().length);
+            if (element.getValue().length > 1) {
+                ut.setCookie(element.getKey(), StringUtils.join(element.getValue()).replace(",", ":"));
+            } else {
+                System.out.println(1);
+                ut.setCookie(element.getKey(), element.getValue()[0]);
+            }
+        }
+        return "redirect:/cookies";
     }
 }
--- src/main/java/com/akim/spring/util/SuumoRequestUtil.java
+++ src/main/java/com/akim/spring/util/SuumoRequestUtil.java
@@ -5,10 +5,10 @@ import org.springframework.web.context.annotation.RequestScope;
 
 import com.akim.spring.common.SuumoCommon;
 import com.akim.spring.common.SuumoCommon.Variables;
-import com.akim.spring.controller.ChintaiAreaController001;
 import com.akim.spring.dto.AreaPrefectureDTO;
 import com.akim.spring.service.TransportationInfoService;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
@@ -62,5 +62,100 @@ public class SuumoRequestUtil extends SuumoCommon.Variables {
         String[] controller = classname.split("[.]");
         session.setAttribute(CONTROLLER,
                 controller.length != 0 ? controller[controller.length - 1] : null);
+    }
+
+    public String getAllCookieValuesAsString() {
+        StringBuilder sb = new StringBuilder();
+
+        Cookie[] cookies = rq.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                sb.append(cookie.getName()).append(": ").append(cookie.getValue()).append("\n");
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public String getAllSessionValuesAsString() {
+        StringBuilder sb = new StringBuilder();
+
+        java.util.Enumeration<String> attributeNames = session.getAttributeNames();
+        while (attributeNames.hasMoreElements()) {
+            String attributeName = attributeNames.nextElement();
+            sb.append(attributeName).append(": ").append(session.getAttribute(attributeName))
+                    .append("\n");
+        }
+
+        return sb.toString();
+    }
+
+
+    // 세션 관련 함수
+    public void setSession(String name, Object value) {
+        session.setAttribute(name, value);
+    }
+
+    private Object getSession(String name, Object defaultValue) {
+        Object value = session.getAttribute(name);
+
+        if (value == null) {
+            return defaultValue;
+        }
+
+        return value;
+    }
+
+    private long getSessionAsLong(String name, long defaultValue) {
+        Object value = getSession(name, null);
+
+        if (value == null)
+            return defaultValue;
+
+        return (long) value;
+    }
+
+    public void removeSession(String name) {
+        session.removeAttribute(name);
+    }
+
+    // 쿠키 관련
+    public void setCookie(String name, String value) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setPath("/");
+        rs.addCookie(cookie);
+    }
+
+    private String getCookie(String name, String defaultValue) {
+        Cookie[] cookies = rq.getCookies();
+
+        if (cookies == null) {
+            return defaultValue;
+        }
+
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals(name)) {
+                return cookie.getValue();
+            }
+        }
+
+        return defaultValue;
+    }
+
+    private long getCookieAsLong(String name, int defaultValue) {
+        String value = getCookie(name, null);
+
+        if (value == null) {
+            return defaultValue;
+        }
+
+        return Long.parseLong(value);
+    }
+
+    public void removeCookie(String name) {
+        Cookie cookie = new Cookie(name, "");
+        cookie.setMaxAge(0);
+        cookie.setPath("/");
+        rs.addCookie(cookie);
     }
 }
